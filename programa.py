8ff5a3b12686ad9ef75fdfdfbab27f354f8fffc0
Rocio <rochidebeliuch@gmail.com>
2017-06-16 11:48:21 -0300

    trabajo 1 clases+imports(sin pygames)

--- programa.py
+++ programa.py
@@ -1,4 +1,45 @@
 from PIL import Image
 import pymysql
-import pygame
+import pygame,sys
 from pygame.locals import *
+import random
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+#db=pymysql.connect(host="127.0.0.1",user="root",password="expo2017",db="expo2017",autocomit=True)
+
+"""def cargarimagenes():
+    foto=None
+    c=db.cursor()
+    c.execute("select ruta from fotosorpresa;")
+    for ruta in c:
+        foto=Image.open(ruta)
+
+    listadeimagenessorpresa.append(foto)"""
+
+#def mostrarimagensorpresa(pos):
+def boton():
+    return True
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode()
+    pygame.display.set_caption("Sorpresa")
+
+    while True:
+        # Posibles entradas del teclado y mouse
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        if boton()==True:
+            #opcionsorpresa=random.randrange(1,len(listadeimagenessorpresa))
+            opcionsorpresa=1
+            if opcionsorpresa ==1:
+                fondo = pygame.image.load("/home/alumno/Descargas/images.jpeg").convert()
+
+                screen.blit(fondo, (0, 0))# Indicamos la posicion de las "Surface" sobre la ventana
+                pygame.display.flip()# se muestran lo cambios en pantalla
+
+main()
