44bc474fa672eded157facf6a638d160db8e21f8
Rocio <rochidebeliuch@gmail.com>
2017-06-29 11:59:07 -0300

    trabajo 1 clases+imports(sin pygames)

--- programa.py
+++ programa.py
@@ -1,25 +1,27 @@
 from PIL import Image
 import pymysql
-#import pygame,simport easygui as eg
 from pygame.locals import *
 import random
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-db=pymysql.connect(host="127.0.0.1",user="root",password="expo2017",db="expo2017",autocomit=True)
+db=pymysql.connect(host="172.16.2.250",user="root",password="alumno",db="las_chicas",autocomit=True)
 
 def exportarimagenesbd(numero):
     foto=None
     c=db.cursor()
-    c.execute("select fotosorpresa from sorpresa where idsorpresa=numero;")#crear procedure para abrir imagen
+    consulta="select fotosorpresa from sorpresa where idsorpresa="+str(numero)+";"
+    c.execute(consulta)
+    #crear procedure para abrir imagen
     for ruta in c:
-        foto=Image.open(ruta)
+        foto=ruta
+    return foto
 
 #guardar imagen(inserts)(guardar en el disco)
 #contar fotos de la base
 
-def guardarimagenendisco(foto):
+"""def guardarimagenendisco(foto):
     eg.msgbox(foto, "fileopenbox", ok_button="Continuar")
     extension = ["*.py", "*.pyc"]
     foto = eg.filesavebox(msg="Guardar archivo",
@@ -28,11 +30,11 @@ def guardarimagenendisco(foto):
                              filetypes=extension
                              )
 
-    eg.msgbox(foto, "filesavebox", ok_button="Continuar")
+    eg.msgbox(foto, "filesavebox", ok_button="Continuar")"""
 
-def guardarimagenenbd(foto):
+def guardarimagenenbd(rutafoto):
     c=db.cursor()
-    c.execute("insert into personas_fototomada fototomada(foto)")
+    c.execute('insert into personas_fototomada values ('rutafoto');')
 
 
 def boton():
