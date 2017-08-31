9af79fbe081d17aa432d3fb4faecf978a2792d68
pi <florenciacuccarese@gmail.com>
2017-08-31 11:28:48 -0300

    hola

--- pantallaenblanco.py
+++ pantallaenblanco.py
@@ -12,16 +12,17 @@ def main():
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((250, 250, 250))
-
+    screen.blit(background, (0, 0))
     # Display some text
     #font = pygame.font.Font(None, 36)
     #text = font.render("Hello There", 1, (10, 10, 10))
     #textpos = text.get_rect()
     #textpos.centerx = background.get_rect().centerx
     #background.blit(text, textpos)
-
+    fondo = pygame.image.load(ruta).convert()
+    screen.blit(fondo, (0, 0))
     # Blit everything to the screen
-    screen.blit(background, (0, 0))
+
     pygame.display.flip()
 
     # Event loop
