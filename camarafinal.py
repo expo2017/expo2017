9c9043fb70fab2dc0f0de30532ad450f1f0ae217
pi <florenciacuccarese@gmail.com>
2017-09-07 11:36:01 -0300

    full screen

--- camarafinal.py
+++ camarafinal.py
@@ -5,11 +5,11 @@ from PIL.ImageOps import *
 
 pygame.camera.init()
 print(pygame.camera.list_cameras())
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],(430,530))#,(640,480)
 cam.start()
 img = cam.get_image()
-pygame.image.save(img, "/home/alumno/Imágenes/fotopersona/photo6.jpg")
-img = Image.open("/home/alumno/Imágenes/fotopersona/photo4.jpg")
+pygame.image.save(img, "/home/pi/Pictures/hola10.jpg")
+img = Image.open("/home/pi/Pictures/hola10.jpg")
 mirror(img).show()#cada vez que se muestra la imagen espejar
 
 pygame.camera.quit()
