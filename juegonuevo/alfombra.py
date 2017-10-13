e6835a4d08994648371d2768414e27f2f38035aa
florenciacuccarese@gmail.com <13dejulio>
2017-10-13 15:27:33 -0300

    juego casi completo

---
+++ juegonuevo/alfombra.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == True:
+         Lo que tengas que hacer
+    time.sleep(0.2)
+
+
+PyAutoGUI
