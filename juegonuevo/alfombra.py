cfcd7a0b59915cd8f30b3e2a6fcff9352473fc1f
flor <florenciacuccarese@gmail.com>
2017-10-13 18:48:25 +0000

    hola

--- juegonuevo/alfombra.py
+++ juegonuevo/alfombra.py
@@ -3,12 +3,17 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)#izquierda
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)#abajo
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)#arriba
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)#derecha
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)#start
+
 
 while True:
     input_state = GPIO.input(18)
     if input_state == True:
-         Lo que tengas que hacer
+         #Lo que tengas que hacer
     time.sleep(0.2)
 
 
