65172aec5e878cf34c896ce52564ba34198a0e1f
flor <florenciacuccarese@gmail.com>
2017-10-19 13:35:02 +0000

    hola

--- juegonuevo/alfombra.py
+++ juegonuevo/alfombra.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-#import pyautogui
+import pyautogui
 
 GPIO.setmode(GPIO.BCM)
 
@@ -14,26 +14,25 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)#start
 while True:
     input_state = GPIO.input(26)
     if input_state == False:
-        #press('left')
+        pyautogui.press('left')
         print("se presiono izquierda")
-         #Lo que tengas que hacer
     input_state = GPIO.input(13)
     if input_state == False:
-        # press('down')
+        pyautogui.press('down')
         print("se presiono abajo")
 
     input_state = GPIO.input(6)
     if input_state == False:
-        # press('up')
+        pyautogui.press('up')
         print("se presiono arriba")
 
     input_state = GPIO.input(5)
     if input_state == False:
-        # press('right')
+        pyautogui.press('right')
         print("se presiono derecha")
     input_state = GPIO.input(16)
     if input_state == False:
-        # press('tab')
+        pyautogui.press('tab')
         print("se presiono start")
 
     time.sleep(1.4)
